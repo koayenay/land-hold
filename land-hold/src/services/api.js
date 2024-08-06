8d1cfe54af71cd66c8f4e1aa8e97d525e7a71457
koayenay <arfan.myint.607@my.csun.edu>
2024-08-06 07:37:41 -0700

    for deployment

--- land-hold/src/services/api.js
+++ land-hold/src/services/api.js
@@ -1,10 +1,16 @@
 import axios from "axios"
 
+// console.log(process.env.REACT_APP_BACKEND_URL)
+// const api = axios.create({
+//   baseURL: "http://localhost:5000/api",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// })
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: process.env.REACT_APP_BACKEND_URL,
   headers: {
     "Content-Type": "application/json",
   },
 })
-
 export default api
