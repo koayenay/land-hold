73b3f11f4a1dac2e097c8c7c00aa62e6065ce143
koayenay <arfan.myint.607@my.csun.edu>
2024-08-06 08:22:00 -0700

    for deployment

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,10 @@ dotenv.config()
 
 const app = express()
 connectDB()
-
+// Root route
+app.get("/", (req, res) => {
+  res.send("Welcome to the backend API!")
+})
 app.use(cors())
 app.use(express.json())
 app.use("/api/auth", authRoutes)
