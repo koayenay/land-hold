7908e8b2890c2de36d6a08ab8dc1664770aae7f5
koayenay <arfan.myint.607@my.csun.edu>
2024-08-06 08:44:14 -0700

    for deployment

--- land-hold/src/services/api.js
+++ land-hold/src/services/api.js
@@ -1,16 +1,16 @@
 import axios from "axios"
 // const dotenv = require("dotenv")
 // console.log(process.env.REACT_APP_BACKEND_URL)
-const api = axios.create({
-  baseURL: "http://localhost:5000/api",
-  headers: {
-    "Content-Type": "application/json",
-  },
-})
 // const api = axios.create({
-//   baseURL: "https://backend-ygez.onrender.com/api",
+//   baseURL: "http://localhost:5000/api",
 //   headers: {
 //     "Content-Type": "application/json",
 //   },
 // })
+const api = axios.create({
+  baseURL: "https://backend-ygez.onrender.com/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
+})
 export default api
